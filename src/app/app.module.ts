e1a238b377b36130ce954e66b2d4033a976116a3
Rachel Lee <rachellee@digitalocean.com>
2021-12-06 08:55:27 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PortfolioModule } from './portfolio/portfolio.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScullyLibModule,
+    NgbModule,
+    PortfolioModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
